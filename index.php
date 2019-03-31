e9c5b4151644ec019763bd487026fb00d39f28ce
Buhanov-Dima <35217770+Buhanov-Dima@users.noreply.github.com>
2019-03-31 06:04:33 +0300

    main 1_1

---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Kit Kraken</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+
+<header>
+	<div class="wrap_head">
+		<div class="menu">
+			<a href="#">меню</a>
+		</div>
+		<div class="logo">
+			<img src="/img/logo.png" alt="">
+		</div>
+		<div class="nmb_user_basket d-flex">
+			<p>+7 495 780 65 65</p>
+			<a href="#"><img src="/img/assets/user.png" alt="user"></a>
+			<a href="#"><img src="/img/assets/shopping-bag.png" alt="basket"></a>
+		</div>
+	</div>
+
+	<div class="left_side">
+		<a href="#"><img src="/img/assets/fb.png" alt=""></a>
+		<a href="#"><img src="/img/assets/inst.png" alt=""></a>
+		<p>сделано для людей с любовью</p>
+	</div>
+
+
+	<div class="scroll_arrow">
+		<p>скрольте  вниз</p>
+	</div>
+
+	<div class="scroll_bar">
+		<p class="sb_1 active">01</p>
+		<p class="sb_2">02</p>
+		<p class="sb_3">03</p>
+		<p class="sb_4">04</p>
+		<p class="sb_5">05</p>
+		<p class="sb_6">06</p>
+	</div>
+
+</header>
+
+<section class="content">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-10 mx-auto">
+				<h1 class="main_h">Кварцевые<br>Моики</h1>
+			</div>
+			<div class="col-lg-5 mx-auto">
+				<div class="kit_review">
+					<a href="#">Выбрать мойку</a>
+					<a href="#">Перейти в каталог</a>
+					<a href="#">Собрать свой кит</a>
+				</div>
+			</div>
+			<div class="col-lg-12 mx-auto m_img">
+				<img src="/img/main/1.svg" alt="">
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<section>
+	<div class="content d-none">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12"></div>
+			</div>
+		</div>
+	</div>
+</section>
+
+	<section></section>
+
+	<section></section>
+
+	<section></section>
+
+	<section></section>
+
+	<section></section>
+
+	<section></section>
+
+	<footer>
+		<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	</footer>
+</body>
+</html>
