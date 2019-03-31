c6ff74250ee59c53880fa5c4fd92c2527f66f2be
piterrus0102 <piterrus@mail.ru>
2019-04-01 01:11:44 +0300

    половина 2.0 конструктора

--- constructor.php
+++ constructor.php
@@ -4,36 +4,88 @@
 	<meta charset="UTF-8">
 	<title>Kit Kraken</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<link rel="stylesheet" href="/css/style.css">
+	<link rel="stylesheet" href="/css/style2.css">
 </head>
 <body>
-
 <header>
-	<div class="wrap_head">
-		<div class="menu">
+	<div class="wrap_head_white">
+		<div class="menu_white">
 			<a href="#">меню</a>
 		</div>
-		<div class="logo">
+		<div class="logo_white">
 			<img src="/img/constructor/logo2.png" alt="">
 		</div>
-		<div class="nmb_user_basket d-flex">
+		<div class="nmb_user_basket_white">
 			<p>+7 495 780 65 65</p>
-			<a href="#"><img src="/img/constructor/user.png" alt="user"></a>
+			<a href="#" id="first_link"><img src="/img/constructor/user.png" alt="user"></a>
 			<a href="#"><img src="/img/constructor/shopping-bag.png" alt="basket"></a>
 		</div>
 	</div>
-
-	<div class="scroll_bar">
-		<p class="sb_1 active">01</p>
-		<p class="sb_2">02</p>
-		<p class="sb_3">03</p>
-		<p class="sb_4">04</p>
-		<p class="sb_5">05</p>
-		<p class="sb_6">06</p>
-	</div>
 </header>
-
-
+<section id="bg">
+	<div class="content line_process">
+		<div class="line_process">
+			<div class="plus" id="first_plus">
+				<img src="/img/constructor/plus.png" alt="plus">
+			</div>
+			<div class="line bigger"></div>
+			<div class="plus">
+				<img src="/img/constructor/plus.png" alt="plus">
+			</div>
+			<div class="line"></div>
+			<div class="plus">
+				<img src="/img/constructor/plus.png" alt="plus">
+			</div>
+			<div class="line"></div>
+			<div class="plus">
+				<img src="/img/constructor/plus.png" alt="plus">
+			</div>
+			<div class="line"></div>
+			<div class="plus">
+				<img src="/img/constructor/plus.png" alt="plus">
+			</div>
+			<div class="line bigger"></div>
+			<div class="dotes">
+				<img src="/img/constructor/dotes.png" alt="dotes">
+			</div>
+		</div>
+	</div>
+	<div class="content notes">
+		<div class="notes">
+			<div id="one_div"><p>кухонная мойка</p></div>
+			<div id="two_div"><p>разделочная доска</p></div>
+			<div id="three_div"><p>подставка</p></div>
+			<div id="four_div"><p>столешница</p></div>
+			<div id="five_div"><p>кухонный фартук</p></div>
+			<div id="six_div"><p>ваш кит	33 848 Р</p></div>
+		</div>	
+	</div>
+	<div class="content choose">
+		<div class="choose">
+		<div class="left_arrow">
+			<img src="/img/constructor/left_arrow.png" alt="left_arrow">
+		</div>
+		<div class="name">
+			<p id="name_name">Sea</p>
+			<p>8 199 рублей</p>
+			<div class="colors">
+				<div></div>
+				<div><img src="/img/constructor/dark_blue.png" alt="dark_blue"></div>
+				<div><img src="/img/constructor/white.png" alt="white"></div>
+				<div><img src="/img/constructor/brown.png" alt="brown"></div>
+				<div><img src="/img/constructor/black.png" alt="black"></div>
+			</div>
+		</div>
+		<div id="info">
+			<img src="/img/constructor/info.png" alt="info">
+			<p>Параметры</p>
+		</div>
+		<div class="right_arrow">
+			<img src="/img/constructor/right_arrow.png" alt="right_arrow">
+		</div>
+	</div>
+	</div>
+</section>
 	<footer>
 		<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
